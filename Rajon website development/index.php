507055a1b7f8bc20ec92a610bd526091661452af
akash-kumar-basak <akash4116@gamil.com>
2021-06-17 22:34:27 +0600

    add all files

---
+++ Rajon website development/index.php
@@ -0,0 +1,13 @@
+<!--PROGRAMMING BY AKASH KUMAR BASAK-->
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> Rshop </title> 
+</head>
+
+<body>
+<header><img src="user/asset/banner1.jpg" width="100%" height="100in"> </header>
+<footer>all rights reserved</footer>
+</body>
+</html>
